0c447a5a4de9643ae833ee45837a01d3575af47c
Emil Urechean <emil.urechean@gmail.com>
2021-02-02 13:57:56 +0100

    fixed bugs

--- azurerm_storage_account/main.tf
+++ azurerm_storage_account/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "terraform_azurerm_automation_demo_storage_account" {
   name                     = "terraform_azurerm_automation_demo_storage_account"
-  resource_group_name      = data.azurerm_resource_group.terraform_azurerm_automation_demo_group.outputs.name
-  location                 = data.azurerm_resource_group.terraform_azurerm_automation_demo_group.outputs.location
+  resource_group_name      = data.azurerm_resource_group.terraform_azurerm_automation_demo_group.name
+  location                 = data.azurerm_resource_group.terraform_azurerm_automation_demo_group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
--- azurerm_storage_container/main.tf
+++ azurerm_storage_container/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_container" "terraform_azurerm_automation_demo_storage_container" {
   name                  = "terraform_azurerm_automation_demo_storage_container"
-  storage_account_name  = data.azurerm_storage_account.terraform_azurerm_automation_demo_storage_account.outputs.name
+  storage_account_name  = data.azurerm_storage_account.terraform_azurerm_automation_demo_storage_account.name
   container_access_type = "private"
 }
