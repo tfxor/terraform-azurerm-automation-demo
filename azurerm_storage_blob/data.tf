42c8f6742eba927af5d6769e8a631b47442e59b1
Emil Urechean <emil.urechean@gmail.com>
2021-02-02 18:04:00 +0100

    trriger

--- azurerm_storage_blob/data.tf
+++ azurerm_storage_blob/data.tf
@@ -3,6 +3,7 @@ data "azurerm_storage_account" "terraform_azurerm_automation_demo_storage_accoun
   resource_group_name = "terraform-azurerm-automation-demo-resource-group"
 }
 
+
 data "azurerm_storage_container" "terraform_azurerm_automation_demo_storage_container" {
   name                 = "azurermautomationdemocontainer"
   storage_account_name = "armautomationdemoaccount"
